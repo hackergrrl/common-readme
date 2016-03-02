a8dc73287980a492da1d73e50ca8dc89066af47a
Stephen Whitmore <stephen.whitmore@gmail.com>
2016-03-02 13:20:32 -0800

    make it work

--- cli.js
+++ cli.js
@@ -2,5 +2,11 @@
 
 var spawn = require('child_process').spawn
 
-var s = spawn('cat', [__dirname + '/' + 'template.md', '|', 'sed', '"s/REPO/${1}/g"'])
+if (process.argv.length !== 3) {
+  console.log('USAGE: generate-readme REPO')
+  process.exit(0)
+}
+
+var cmd = 'cat ' + __dirname + '/template.md | sed "s/REPO/' + process.argv[2] + '/g"'
+var s = spawn('sh', ['-c', cmd])
 s.stdout.pipe(process.stdout)
